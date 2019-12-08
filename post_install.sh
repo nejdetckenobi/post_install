746864b94762bb0ec2a23cc3e006d96c3d569714
nejdetckenobi <nejdetyucesoy@gmail.com>
2019-12-08 16:59:09 +0300

    Refreshed.

---
+++ post_install.sh
@@ -0,0 +1,15 @@
+apt update
+apt upgrade
+apt install -y \
+	git \
+	git-core \
+	zsh \
+	peek \
+	flameshot \
+	vim \
+	tmux \
+	curl \
+	wget
+
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
